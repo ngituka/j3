b2c76e9aa853c6643825020bed771e17963d82f3
Jannie Ngoy Ituka <janniengoyituka@icloud.com>
2026-01-28 11:36:50 +0100

    tailles responsive, deux valeurs, changement du grid

--- script/index.js
+++ script/index.js
@@ -2,11 +2,25 @@ function getTime(){
     let today = new Date();
     let wedding = new Date(2026,7,8,11,0,0);
     let diff = wedding-today;
-    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-    console.log("days : " + days + "hours : " + hours + "minutes + " + minutes + "seconds : " + seconds)
-    document.querySelector(".countdown").innerHTML = days + "j " + hours + "h " + minutes + "m " + seconds + "s"
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000).toString();
+    if (days.length == 1){
+        days = "0" + days;
+    }
+    if (hours.length == 1){
+        hours = "0" + hours;
+    }
+    if (minutes.length == 1){
+        minutes = "0" + minutes;
+    }
+    if (seconds.length == 1){
+        seconds = "0" + seconds;
+    }
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".mins").innerHTML = minutes;
+    document.querySelector(".secs").innerHTML = seconds;
 }   
 setInterval(getTime, 1000);
