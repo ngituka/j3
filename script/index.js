8abc7d0539cb9c57c53d1c4ea9c3ddb6b49dcebc
Jannie Ngoy Ituka <janniengoyituka@icloud.com>
2026-01-29 21:32:31 +0100

    commit accidentel

--- script/index.js
+++ script/index.js
@@ -2,21 +2,34 @@ function getTime(){
     let today = new Date();
     let wedding = new Date(2026,7,8,11,0,0);
     let diff = wedding-today;
-    let days = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
-    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
-    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
-    let seconds = Math.floor((diff % (1000 * 60)) / 1000).toString();
-    if (days.length == 1){
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    if (days.toString().length == 1){
         days = "0" + days;
     }
-    if (hours.length == 1){
+    if (hours.toString().length == 1){
         hours = "0" + hours;
     }
-    if (minutes.length == 1){
+    if (minutes.toString().length == 1){
         minutes = "0" + minutes;
     }
-    if (seconds.length == 1){
+    if (seconds.toString().length == 1){
         seconds = "0" + seconds;
+    }
+
+    if(days<0){
+        days = "00";
+    }
+    if(hours<0){
+        hours = "00";
+    }
+    if(minutes<0){
+        minutes = "00";
+    }
+    if(seconds<0){
+        seconds = "00";
     }
     document.querySelector(".days").innerHTML = days;
     document.querySelector(".hours").innerHTML = hours;
